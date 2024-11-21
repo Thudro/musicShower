761c087936ad1732b9ce23509de72b59f5068d05
Thudro <blakeford1202@live.co.uk>
2024-11-21 00:25:53 +0000

    Added a lot of changes as I forgot to commit. Added HTML, client now acts as a flask server, added album art, added coments for myself

--- Client/main.py
+++ Client/main.py
@@ -1,16 +1,51 @@
 import requests
+from flask import Flask, render_template
 import time
 
+app = Flask(__name__)
 PC_IP = "192.168.1.1" 
 URL = f"http://{PC_IP}:5000/now-playing"
 
-while True:
+def fetch_now_playing():
     try:
         response = requests.get(URL)
         data = response.json()
-        print(f"Now Playing: {data['now_playing']}")
-
+        return data.get('now_playing', "No song playing")
     except Exception as e:
-        print(f"Error: {e}")#
+        return(str(e))
+
+# Im using itunes as it doesnt need an api key hehe
+def fetch_album_art(song_details):
+
+    # Url dosnt like spaces
+    query = song_details.replace(" ", "+")
+    url = f"https://itunes.apple.com/search?term={query}&entity=song&limit=1"
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+
+        # Ensure something is there
+        if data["resultCount"] > 0:
+            artwork_url = data["results"][0]["artworkUrl100"]
+
+            #Increase resolution to fit pannel
+            artwork_url = artwork_url.replace("100x100", "600x600")
+            return artwork_url
+        else:
+            return None
+    else:
+        return None
+    
+@app.route('/')
+def index():
+    now_playing = fetch_now_playing()
+    album_art_url = fetch_album_art(now_playing)
+    # This is Flask passing now_playing and the album art url to the index.html (Kinda like passing in variables if you want to think of it like that)
+    return render_template('index.html', now_playing=now_playing, album_art_url=album_art_url)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
+
 
 
--- Server/main.py
+++ Server/main.py
@@ -14,14 +14,19 @@ async def get_media_info():
     current_session = sessions.get_current_session()
     if current_session:  # there needs to be a media session running
         info = await current_session.try_get_media_properties_async()
+
+        #This adds ALL the data from the sdk to a dictionary
         info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
-        concat = info_dict['title'] + " By " + info_dict['artist']
+
+        #We only want the title and artist tho
+        concat = info_dict['title'] + " -" + info_dict['artist']
         return concat
 
 
 @app.route('/now-playing', methods=['GET'])
 def now_playing():
     current_media_info = asyncio.run(get_media_info())
+    #We want to use json for the data on the server, so Im using jsonify 
     return jsonify({'now_playing': current_media_info})
 
 if __name__ == '__main__':
